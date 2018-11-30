3c9c605f40919c6d778d8a0414eec46c56c9e1cc
Анна <blackwooddreamer@gmail.com>
2018-11-30 20:42:14 +0200

    fx update 30.5

--- src/sample/controllers/EncController.java
+++ src/sample/controllers/EncController.java
@@ -10,6 +10,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.lang.reflect.Array;
 import java.util.Scanner;
 
 public class EncController {
@@ -45,7 +46,7 @@ public class EncController {
 
         Stage stage = (Stage) ret.getScene().getWindow();
         stage.close();
-    }
+    }}
 
 //    public void Key(){
 //        String keyy;
@@ -54,19 +55,32 @@ public class EncController {
 //    }
 
 
-    public void ClickOnEnc(ActionEvent actionEvent) {
+//    public class ClickOnEnc (ActionEvent actionEvent) {
 //  String keyy;
 //        keyy = key.getText();
-//        String cif;
-//        cif = cipher.getText();
-
-        char[] characters = new char[] { 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И',
-                'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С',
-                'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ы', 'Ъ',
-                'Э', 'Ю', 'Я', ' ','.',',','!','?',';',':','A','B','C','D','E','F','G','H','I','J','K','L','M',
-                'N','O','P','Q','R','S','T','U','V','W','X','Y','Z', '1', '2', '3', '4', '5', '6', '7',
-                '8', '9', '0' };
-
-        int N = characters.length;
-    }
-}
+////        String cif;
+////        cif = cipher.getText();
+//
+//        char[] characters = new char[] { 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И',
+//                'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С',
+//                'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ы', 'Ъ',
+//                'Э', 'Ю', 'Я', ' ','.',',','!','?',';',':','A','B','C','D','E','F','G','H','I','J','K','L','M',
+//                'N','O','P','Q','R','S','T','U','V','W','X','Y','Z', '1', '2', '3', '4', '5', '6', '7',
+//                '8', '9', '0' };
+//
+//        int N = characters.length;
+//        String input = null;
+//        input = input.toUpperCase();
+//        keyy = keyy.toUpperCase();
+//        String result = "";
+//        int keyIndex = 0;
+//        for (char symbol : input) {
+//            int a = (Array.indexOf(characters, symbol)+(Array.indexOf(characters, keyy[keyIndex])))% N;
+//            result += characters[a];
+//            keyIndex++;
+//
+//            if ((keyIndex + 1)== keyy.length()) keyIndex=0;
+//        }
+//        return result;
+//    }
+//}
