3a3875e0699482262c67962ed32700229a50434b
Анна <blackwooddreamer@gmail.com>
2018-11-30 10:27:36 +0200

    fx update 30.1

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,13 +6,21 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("fx/signUp.fxml"));
+    public void start(Stage primaryStage){
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("fx/signUp.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         primaryStage.setTitle("Шифр Вижи");
-        primaryStage.setScene(new Scene(root, 500, 500));
+        primaryStage.setScene(new Scene(root, 600, 400
+        ));
         primaryStage.show();
     }
 
--- src/sample/controllers/SignUpController.java
+++ src/sample/controllers/SignUpController.java
@@ -4,10 +4,15 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+
+
+//import static com.sun.javafx.scene.control.skin.Utils.getResource;
 
 
 public class SignUpController {
@@ -24,21 +29,38 @@ public class SignUpController {
     @FXML
     private Button signIn;
 
-    @FXML
-    void setPass() {
-        pass.setOnAction(event -> {
-        });
-    }
-    @FXML
-    void setLogin(){
-        login.setOnAction(event -> {});
-    }
-    @FXML
-    void setButUp(){
-        signUp.setOnAction(event -> {});
-    }
-    @FXML
-    void setButIn(){
-        pass.setOnAction(event -> {});
+//    @FXML
+//    void setPass() {
+//        pass.setOnAction(event -> {
+//        });
+//    }
+//    @FXML
+//    void setLogin(){
+//        login.setOnAction(event -> {});
+//    }
+//    @FXML
+//    void setButUp(){
+//        signUp.setOnAction(event -> {});
+//    }
+//    @FXML
+//    void setButIn(){
+//        pass.setOnAction(event -> {});
+//    }
+
+    public void onClickSignUp(ActionEvent actionEvent) {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../fx/chooseAct.fxml"));
+
+        try {
+            Parent root1 = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        Stage stage = (Stage) signUp.getScene().getWindow();
+        stage.close();
     }
 }
